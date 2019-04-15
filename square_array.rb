fae9d1a78ad5842cc6a2be69bec4358b59f7cc7a
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-04-15 20:08:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  iterator = 0
+  array.each do |apple|
+    num = array[apple]
+    while iterator < num
+      array[apple] = array[apple]*num
+    end
+    iterator = 0
+  end
 end
